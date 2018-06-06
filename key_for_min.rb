b06fa25657cc98b6d5215cf3cb0d31b6c26f63cf
Adam Barton <adam.barton333@gmail.com>
2018-06-06 04:28:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_key = nil 
   
     name_hash.each do |k, v|
-      if lowest_value == nil && v >
+      if lowest_value == nil || v < lowest_value
     
 
 end
