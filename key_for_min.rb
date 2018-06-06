a2e8f8a10233882852d01b11faf92e87bd769e1d
Adam Barton <adam.barton333@gmail.com>
2018-06-06 04:26:51 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.sort do |x,y|
-     x <=> y
-  end.first[0]
+  lowest_value = nil 
+  lowest_key = nil 
+  
+  if 
     
 
 end
